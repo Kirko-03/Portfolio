4d86a270448de887edfd678e361b95a0c4091635
Kirito <kurulluvarov@gmail.com>
2021-09-27 14:31:54 +0300

    HIRE ME

--- src/components/05_hiring/Hiring.tsx
+++ src/components/05_hiring/Hiring.tsx
@@ -6,7 +6,7 @@ export const Hiring = () => {
       <div className={style.block}>
         <h2>I am considering options for remote work</h2>
         <button className={style.button}>
-          <a href={"https://hh.ru"}>Hire me</a>
+          <a href={"https://hh.ru/resume/7e494630ff08cdb52d0039ed1f557278464e6d"}>Hire me</a>
         </button>
       </div>
     </div>
